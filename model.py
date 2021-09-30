89bc606c71ac8862d9a156f86ea6da5baa79748b
amphineko <sukareki@gmail.com>
2021-09-30 09:34:12 +0100

    re(train): 😫

--- model.py
+++ model.py
@@ -1,26 +1,26 @@
+from tensorflow.keras import *
 from tensorflow.keras.layers import *
-from tensorflow.keras.models import *
 
-from config import max_len, symbols
+default_batch_size, default_epochs, default_step = 32, 5, 100
+default_height, default_width = 64, 128
 
-width, height, n_len, n_class = 128, 64, max_len, len(symbols)
 
-inputs = Input((height, width, 3))
-x = inputs
-for i, n_cnn in enumerate([2, 2, 2, 2, 2]):
-    for j in range(n_cnn):
-        x = Conv2D(32 * 2 ** min(i, 3),
-                   kernel_initializer='he_uniform',
-                   kernel_size=3,
-                   padding='same')(x)
-        x = BatchNormalization()(x)
-        x = Activation('relu')(x)
-    x = MaxPooling2D(2)(x)
+def create_model(captcha_length, n_symbols, input_shape, model_depth=5, module_size=2):
+    input_tensor = Input(input_shape)
 
-x = Flatten()(x)
-x = [Dense(n_class,
-           activation='softmax',
-           name='c%d' % (i+1)
-           )(x) for i in range(n_len)]
+    x = input_tensor
+    for i, module_length in enumerate([module_size] * model_depth):
+        for j in range(module_length):
+            x = Conv2D(32 * 2 ** min(i, 3),
+                       kernel_initializer='he_uniform',
+                       kernel_size=3,
+                       padding='same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+        x = MaxPooling2D(2)(x)
 
-model = Model(inputs=inputs, outputs=x)
+    x = Flatten()(x)
+    x = [Dense(n_symbols, activation='softmax', name='char_%d' % (i+1))(x)
+         for i in range(captcha_length)]
+
+    return Model(inputs=input_tensor, outputs=x)
--- train.py
+++ train.py
@@ -1,48 +1,55 @@
 #!/usr/bin/env python3
 
+import argparse
 import multiprocessing
 import random
+import warnings
+from pathlib import Path
 
 import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
 from captcha.image import ImageCaptcha
 from tensorflow.keras.utils import Sequence
 
-from config import max_len, min_len, symbols
-from model import model
+from model import (create_model, default_batch_size, default_epochs,
+                   default_height, default_width)
 
-len_range = range(min_len, max_len + 1)
-generator = ImageCaptcha(width=128, height=64)
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class CaptchaSequence(Sequence):
-    def __init__(self, symbols, batch_size, steps, width=128, height=64):
+    def __init__(self,
+                 captcha_length,
+                 captcha_symbols,
+                 batch_size,
+                 generator,
+                 width=128,
+                 height=64):
         self.batch_size = batch_size
         self.generator = generator
         self.height = height
-        self.n_class = len(symbols)
-        self.steps = steps
-        self.symbols = symbols
+        self.n_class = len(captcha_symbols)
+        self.n_len = captcha_length
+        self.symbols = captcha_symbols
         self.width = width
 
     def __len__(self):
-        return self.steps
+        return self.batch_size
 
     def __getitem__(self, _):
-        n_len = random.choice(len_range)
-
         X = np.zeros((self.batch_size, self.height, self.width, 3),
                      dtype=np.float32)
         y = [
             np.zeros((self.batch_size, self.n_class), dtype=np.uint8)
-            for _ in range(max_len)
+            for _ in range(self.n_len)
         ]
 
         for i in range(self.batch_size):
-            text = ''.join([
-                random.choice(self.symbols)
-                for _ in range(n_len)
-            ])
-            image = self.generator.generate_image(text.ljust(max_len))
+            text = ''.join(
+                [random.choice(self.symbols) for _ in range(self.n_len)])
+            image = self.generator.generate_image(text)
 
             X[i] = np.array(image) / 255.0
             for j, ch in enumerate(text):
@@ -53,24 +60,82 @@ class CaptchaSequence(Sequence):
 
 
 if __name__ == '__main__':
-    from tensorflow.keras.callbacks import (CSVLogger, EarlyStopping,
-                                            ModelCheckpoint)
-    from tensorflow.keras.optimizers import *
+    parser = argparse.ArgumentParser()
 
-    train_data = CaptchaSequence(symbols, batch_size=128, steps=1000)
-    valid_data = CaptchaSequence(symbols, batch_size=128, steps=100)
-    callbacks = [
-        EarlyStopping(patience=3),
-        CSVLogger('model.csv'),
-        ModelCheckpoint('model_best.h5', save_best_only=True)
-    ]
+    parser.add_argument('--height', default=default_height, type=int)
+    parser.add_argument('--width', default=default_width, type=int)
 
-    model.compile(loss='categorical_crossentropy',
-                  metrics=['accuracy'],
-                  optimizer=Adam(1e-3, amsgrad=True))
-    model.fit_generator(train_data,
-                        callbacks=callbacks,
-                        epochs=100,
-                        use_multiprocessing=False,
-                        validation_data=valid_data,
-                        workers=multiprocessing.cpu_count())
+    parser.add_argument('--batch-size', default=default_batch_size, type=int)
+    parser.add_argument('--epochs', default=default_epochs, type=int)
+    parser.add_argument('--validation-size',
+                        default=default_batch_size,
+                        type=int)
+
+    parser.add_argument('--captcha-length', type=int)
+    parser.add_argument('--model-name', type=str)
+    parser.add_argument('--symbols', default='symbols.txt', type=str)
+
+    parser.add_argument('--gpu', type=bool)
+
+    args = parser.parse_args()
+
+    batch_size, epochs, validation_size = args.batch_size, args.epochs, args.validation_size
+
+    symbols_path = Path(args.symbols)
+    if not symbols_path.is_file():
+        raise IOError('Symbol file is not exist: ' + symbols_path)
+    symbols = symbols_path.read_text()
+
+    checkpoint_path = Path(args.model_name).with_suffix('.h5')
+    logs_path = Path(args.model_name).with_suffix('.csv')
+    resume_path = Path(args.model_name).with_suffix('.resume.h5')
+
+    captcha_generator = ImageCaptcha(width=128, height=64)
+
+    if args.gpu:
+        device = tf.device('/device:GPU:0')
+
+        # tensorflow gpu memory fix
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+    else:
+        device = tf.device('/device:CPU:0')
+
+    with device:
+        model = create_model(args.captcha_length, len(symbols),
+                             (args.height, args.width, 3))
+
+        if checkpoint_path.is_file():
+            print('Loading checkpint from ' + checkpoint_path.name)
+            model.load_weights(checkpoint_path)
+
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=keras.optimizers.Adam(1e-3, amsgrad=True),
+                      metrics=['accuracy'])
+
+        training_data = CaptchaSequence(args.captcha_length,
+                                        symbols,
+                                        batch_size=batch_size,
+                                        generator=captcha_generator,
+                                        height=args.height,
+                                        width=args.width)
+        validation_data = CaptchaSequence(args.captcha_length,
+                                          symbols,
+                                          batch_size=validation_size,
+                                          generator=captcha_generator,
+                                          height=args.height,
+                                          width=args.width)
+
+        callbacks = [
+            keras.callbacks.EarlyStopping(patience=3),
+            keras.callbacks.CSVLogger(logs_path, append=True),
+            keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                            save_best_only=True)
+        ]
+
+        model.fit(training_data,
+                  callbacks=callbacks,
+                  epochs=epochs,
+                  use_multiprocessing=True,
+                  validation_data=validation_data,
+                  workers=multiprocessing.cpu_count())
